ff5c82e974b685418ba680da1106b163f689a412
eulis01 <eulis01@gmail.com>
2020-11-16 16:23:08 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: 'Super clean code', description: 'Rails allow for super clean coding habits👍')
